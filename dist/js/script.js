ebea7861280f6d32cb0fc2bc9ba42cc8ab099e1a
Fayzullo <fayzullarakhmatullayev@gmail.com>
2025-09-13 15:58:23 +0500

    feat: slider & shop sections

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,14 +1,19 @@
-const swiper = new Swiper('.slider-swiper', {
-  direction: 'horizontal',
+const { createApp, ref } = window.Vue;
+const app = createApp({
+  setup() {
+    return {};
+  }
+});
+app.mount('#app');
 
-  // gap
+new Swiper('.slider-swiper', {
+  direction: 'horizontal',
   slidesPerView: 'auto',
   spaceBetween: 16,
-
   pagination: {
-    el: '.swiper-pagination'
+    el: '.swiper-pagination',
+    clickable: true
   },
-
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev'
