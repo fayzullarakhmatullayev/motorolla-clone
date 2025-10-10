1b267a4a1a4f92cb2ff8753ff6e521c29b1d922b
Fayzulla Raxmatullayev <fayzullarakhmatullayev@gmail.com>
2025-10-10 11:17:14 +0500

    update images

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,33 +1,33 @@
 const SHOPS = {
   photografers: [
-    { id: 1, img: 'img/shops/shop-1.png', title: 'motorola edge 60 pro' },
-    { id: 2, img: 'img/shops/shop-2.png', title: 'motorola edge 60 fusion' },
-    { id: 3, img: 'img/shops/shop-3.png', title: 'motorola edge 50 ultra' },
-    { id: 4, img: 'img/shops/shop-4.png', title: 'motorola edge 50 pro' }
+    { id: 1, img: 'img/shops/shop-1.png', title: 'edge 60 pro' },
+    { id: 2, img: 'img/shops/shop-2.png', title: 'edge 60 fusion' },
+    { id: 3, img: 'img/shops/shop-3.png', title: 'edge 50 ultra' },
+    { id: 4, img: 'img/shops/shop-4.png', title: 'edge 50 pro' }
   ],
   gamers: [
-    { id: 5, img: 'img/shops/shop-5.png', title: 'motorola edge 60 pro' },
-    { id: 6, img: 'img/shops/shop-6.png', title: 'motorola edge 50 pro' },
-    { id: 7, img: 'img/shops/shop-7.png', title: 'moto g85 5g' },
-    { id: 8, img: 'img/shops/shop-8.png', title: 'moto g55 5G' }
+    { id: 5, img: 'img/shops/shop-5.png', title: 'edge 60 pro' },
+    { id: 6, img: 'img/shops/shop-6.png', title: 'edge 50 pro' },
+    { id: 7, img: 'img/shops/shop-7.png', title: 'g85 5g' },
+    { id: 8, img: 'img/shops/shop-8.png', title: 'g55 5G' }
   ],
   content_creators: [
-    { id: 9, img: 'img/shops/shop-9.png', title: 'motorola razr 60 ultra' },
-    { id: 10, img: 'img/shops/shop-10.png', title: 'motorola razr 60' },
-    { id: 11, img: 'img/shops/shop-11.png', title: 'motorola razr 50 ultra' },
-    { id: 12, img: 'img/shops/shop-12.png', title: 'motorola razr 50' }
+    { id: 9, img: 'img/shops/shop-9.png', title: 'razr 60 ultra' },
+    { id: 10, img: 'img/shops/shop-10.png', title: 'razr 60' },
+    { id: 11, img: 'img/shops/shop-11.png', title: 'razr 50 ultra' },
+    { id: 12, img: 'img/shops/shop-12.png', title: 'razr 50' }
   ],
   adventures: [
-    { id: 13, img: 'img/shops/shop-13.png', title: 'moto g85 5g' },
-    { id: 14, img: 'img/shops/shop-14.png', title: 'moto g55 5G' },
-    { id: 15, img: 'img/shops/shop-15.png', title: 'moto g35 5G' },
-    { id: 16, img: 'img/shops/shop-16.png', title: 'moto g24' }
+    { id: 13, img: 'img/shops/shop-13.png', title: 'g85 5g' },
+    { id: 14, img: 'img/shops/shop-14.png', title: 'g55 5G' },
+    { id: 15, img: 'img/shops/shop-15.png', title: 'g35 5G' },
+    { id: 16, img: 'img/shops/shop-16.png', title: 'g24' }
   ]
 };
 
 const BLOGS = [
-  { id: 1, img: 'img/blogs/blog-1.jpg', title: 'Motorola joins \nEuroLeague action' },
-  { id: 2, img: 'img/blogs/blog-2.jpg', title: 'Motorola joins the \nFormula 1 grid' },
+  { id: 1, img: 'img/blogs/blog-1.jpg', title: 'Joins \nEuroLeague action' },
+  { id: 2, img: 'img/blogs/blog-2.jpg', title: 'Joins the \nFormula 1 grid' },
   { id: 3, img: 'img/blogs/blog-3.jpg', title: 'Driving change with \nBounce for Good' },
   { id: 4, img: 'img/blogs/blog-4.jpg', title: 'Express yourself \nwith Pantone' }
 ];
